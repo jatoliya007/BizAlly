1c4a5cfb7ca24cb672e6af986b9b1dd805370aee
jatoliya007 <regar.deepika7@gmail.com>
2024-04-21 12:21:06 +0530

    latest

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-function navHoverContent(contentId){
-    console.log('navHoverContent ', contentId);
+// function navHoverContent(contentId){
+//     console.log('navHoverContent ', contentId);
 
-let linksContainer = document.getElementById("links-container");
-}
+// let linksContainer = document.getElementById("links-container");
+
+// let linkOne = document.getElementsByClassName("link-one");
+// for(let i = 0; i < linkOne; i++);
+
+// linkOne[i].style.display = "none";
+
+// }
+
+// if(contentId){
+//     document.getElementById(contentId).style.display = "block";
+
+// }
+// else{
+//     document.getElementById(contentId).style.display = "block";
+
+// }
