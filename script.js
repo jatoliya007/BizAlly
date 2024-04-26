9fd6be98a910dbb5a6f75b810609f1b154e48b1b
jatoliya007 <regar.deepika7@gmail.com>
2024-04-26 14:21:16 +0530

    latet

--- script.js
+++ script.js
@@ -2,13 +2,19 @@ function myFunction (){
 
     let mydiv = document.getElementById("myDiv");
 
+    let menuIcon = document.getElementById("mobile-menu-icon");
+    menuIcon.classList.remove("fa-bars");
+    menuIcon.classList.remove("fa-close");
+
     if (mydiv.style.display === "block"){
 
         mydiv.style.display = "none";
+        menuIcon.classList.add("fa-bars");
 
     } else {
 
         mydiv.style.display = "block";
+        menuIcon.classList.add("fa-close");
         
     }
 }
@@ -24,10 +30,32 @@ function onMenuItemClick(evt){
         evt.nextElementSibling.querySelectorAll('ul').forEach(x => {
             x.style.display = "block"
         })
+        let icon = evt.querySelector('.fa-chevron-down');
+        icon.classList.remove('fa-chevron-down')
+        icon.classList.add('fa-chevron-up')
     }else{
         evt.nextElementSibling.style.display = "none";
         evt.nextElementSibling.querySelectorAll('ul').forEach(x => {
             x.style.display = "none"
         })
+        let icon = evt.querySelector('.fa-chevron-up');
+        icon.classList.remove('fa-chevron-up')
+        icon.classList.add('fa-chevron-down')
     }
+}
+
+// for scrolling navbar fixed
+window.onscroll = function() {myscroll()};
+
+let fixedbar = document.getElementById("fixed-bar");
+
+let sticky = fixedbar.offsetTop;
+
+function myscroll() {
+    if (window.scrollY >= sticky) {
+        fixedbar.classList.add("sticky")
+} else {
+    fixedbar.classList.remove("sticky");
+}
+
 }
